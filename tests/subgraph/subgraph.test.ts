61f0cfc4bc198ffd8d07632bc5e3b8df4c558330
Michael Chrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2021-12-27 15:35:59 -0500

    mocking and testing the ability to create a new project entity

--- tests/subgraph/subgraph.test.ts
+++ tests/subgraph/subgraph.test.ts
@@ -1,32 +1,27 @@
 import { assert, createMockedFunction, clearStore, test,  newMockCall } from "matchstick-as/assembly/index"
 import { Address, BigInt, Bytes, ethereum, store, Value, log } from "@graphprotocol/graph-ts"
-import { AddWhitelistedCall, 
+import { GenArt721Core,
+         AddProjectCall,
+         AddWhitelistedCall, 
          RemoveWhitelistedCall, 
          AddMintWhitelistedCall, 
          UpdateRandomizerAddressCall,
          UpdateArtblocksAddressCall,
-         UpdateArtblocksPercentageCall
+         UpdateArtblocksPercentageCall,
+         AddProjectScriptCall
          } from '../../generated/GenArt721Core/GenArt721Core';
-import { Contract, Token } from "../../generated/schema";
+import { Contract, Project, Token } from "../../generated/schema";
 import { logStore } from "matchstick-as";
-import { handleAddWhitelisted, 
+import { handleAddProject,
+         handleAddWhitelisted, 
          handleRemoveWhitelisted, 
          handleAddMintWhitelisted, 
          handleUpdateRandomizerAddress,
          handleUpdateArtblocksAddress,
-         handleUpdateArtblocksPercentage } from '../../src/mapping';
-// import data from "../testData.json";
+         handleUpdateArtblocksPercentage,
+         handleAddProjectScript } from '../../src/mapping';
+import { script } from '../meridianScript';
 
-// import { handleNewGravatars, createNewGravatarEvent, trySaveGravatarFromContract, saveGravatarFromContract } from "./utils"
-// import { Gravatar } from "../../generated/schema"
-// import { Gravity, NewGravatar, CreateGravatarCall } from "../../generated/Gravity/Gravity"
-// import { handleCreateGravatar, handleNewGravatar } from "../../src/gravity"
-
-// Coverage
-// export { handleCreateGravatar, handleNewGravatar };
-
-// let GRAVATAR_ENTITY_TYPE = "Gravatar"
-// let TRANSACTION_ENTITY_TYPE = "Transaction"
 let ACCOUNT_ENTITY_TYPE = "Account"
 let PROJECT_ENTITY_TYPE = "Project"
 let CONTRACT_ENTITY_TYPE = "Contract"
@@ -52,35 +47,96 @@ createMockedFunction(coreContractAddress, 'nextProjectId', 'nextProjectId():(uin
 createMockedFunction(coreContractAddress, 'randomizerContract', 'randomizerContract():(address)')
   .returns([ethereum.Value.fromAddress(Address.fromString('0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270'))])
   
-createMockedFunction(coreContractAddress, 'randomizerContract', 'randomizerContract():(address)')
-  .returns([ethereum.Value.fromAddress(Address.fromString('0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270'))])
+let projectDetailsReturnArray: Array<ethereum.Value> = [
+  ethereum.Value.fromString("string1"),     // projectName
+  ethereum.Value.fromString("string2"),     // artist
+  ethereum.Value.fromString("string3"),     // description
+  ethereum.Value.fromString("string4"),     // website
+  ethereum.Value.fromString("string7"),     // license
+  ethereum.Value.fromBoolean(true)];        // dynamic
+createMockedFunction(coreContractAddress, 'projectDetails', 'projectDetails(uint256):(string,string,string,string,string,bool)')
+  .withArgs([ethereum.Value.fromSignedBigInt(BigInt.fromString("99"))])
+  .returns(projectDetailsReturnArray)
+
+let projectTokenInfoReturnArray: Array<ethereum.Value> = [
+  ethereum.Value.fromAddress(Address.fromString("0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270")),   // artistAddress
+  ethereum.Value.fromSignedBigInt(BigInt.fromString("100000000")),                                // pricePerTokenInWei
+  ethereum.Value.fromSignedBigInt(BigInt.fromString("1024")),                                     // invocations
+  ethereum.Value.fromSignedBigInt(BigInt.fromString("1024")),                                     // maxInvocations
+  ethereum.Value.fromBoolean(false),                                                              // active
+  ethereum.Value.fromAddress(Address.fromString("0xc079F0A6E8809E41A2A39D6532ff3dfa6B48e6bB")),   // additionalPayee
+  ethereum.Value.fromSignedBigInt(BigInt.fromString("10")),                                       // additionalPayeePercentage
+  ethereum.Value.fromString("GRT"),                                                               // currency
+  ethereum.Value.fromAddress(Address.fromString("0xc944e90c64b2c07662a292be6244bdf05cda44a7"))];  // currencyAddress
+createMockedFunction(coreContractAddress, 'projectTokenInfo', 'projectTokenInfo(uint256):(address,uint256,uint256,uint256,bool,address,uint256,string,address)')
+.withArgs([ethereum.Value.fromSignedBigInt(BigInt.fromString("99"))])
+  .returns(projectTokenInfoReturnArray)
+
+let projectScriptInfoReturnArray: Array<ethereum.Value> = [
+  ethereum.Value.fromString(script),                                                   // scriptJSON
+  ethereum.Value.fromSignedBigInt(BigInt.fromString("1")),                             // scriptCount
+  ethereum.Value.fromBoolean(true),                                                    // useHashString
+  ethereum.Value.fromString("mtwirsqawjuoloq2gvtyug2tc3jbf5htm2zeo4rsknfiv3fdp46a"),   // IPFSHash
+  ethereum.Value.fromBoolean(true),                                                    // locked
+  ethereum.Value.fromBoolean(true)];                                                   // paused
+createMockedFunction(coreContractAddress, 'projectScriptInfo', 'projectScriptInfo(uint256):(string,uint256,bool,string,bool,bool)')
+.withArgs([ethereum.Value.fromSignedBigInt(BigInt.fromString("99"))])
+  .returns(projectScriptInfoReturnArray)
 }
 
-test("Should throw an error", () => {
-  throw new Error()
-}, true)
+function createProjectToLoad(call: ethereum.Call): void {
+  let contract = GenArt721Core.bind(call.to);
+  let projectEntity = new Project(contract._address.toHexString());
 
-// test("Can call mappings with custom call handlers", () => {
+  projectEntity.save();
+  logStore();
+}
+
+const mockRefreshProjectScript = function(): void {
+
+}
+
+// test("Should throw an error", () => {
 //   let call = changetype<AddProjectCall>(newMockCall())
-//   // let call =  newMockCall() as AddProjectCall
+  
+//   call.to = Address.fromString("0xa7d8d9ef8D8Ce8992Df33D8b8CF4Aebabd5bD270")  // must be 721Core?
+//   let addr = Address.fromString("0x1233973F9aEa61250e98b697246cb10146903672")
+
 //   call.inputValues = [
-//     new ethereum.EventParam("projectName", 
-//     ethereum.Value.fromString("Ringers")), 
-//     new ethereum.EventParam("artistAddress", 
-//     ethereum.Value.fromString("0x1233973F9aEa61250e98b697246cb10146903672")),
-//     new ethereum.EventParam("pricePerTokenInWei", 
-//     ethereum.Value.fromString("123"))
+//     new ethereum.EventParam("_address",
+//     ethereum.Value.fromAddress(addr))
 //   ]
-//   handleAddProject(call)
-//   // testFunc()
-//   // testMe()
+//   createProjectToLoad(call);
+//   throw new Error()
+// }, true)
 
-//   assert.fieldEquals(PROJECT_ENTITY_TYPE, "0x1233973F9aEa61250e98b697246cb10146903672", "projectName", "Ringers")
-//   // assert.fieldEquals(PROJECT_ENTITY_TYPE, "0x1233973F9aEa61250e98b697246cb10146903672", "artistAddress", "0x1233973F9aEa61250e98b697246cb10146903672")
-//   // assert.fieldEquals(PROJECT_ENTITY_TYPE, "0x1233973F9aEa61250e98b697246cb10146903672", "pricePerTokenInWei", "123")
+test("Can add a new project", () => {
+  let call = changetype<AddProjectCall>(newMockCall())
+  call.to = Address.fromString("0xa7d8d9ef8D8Ce8992Df33D8b8CF4Aebabd5bD270")  // must be 721Core?
+  call.block.timestamp = BigInt.fromString('1230')
+  let addr = Address.fromString("0x1233973F9aEa61250e98b697246cb10146903672")
 
-//   // clearStore();
-// });
+  call.inputValues = [
+    new ethereum.EventParam("projectName", 
+    ethereum.Value.fromString("Ringers")), 
+    new ethereum.EventParam("artistAddress", 
+    ethereum.Value.fromString("0x1233973F9aEa61250e98b697246cb10146903672")),
+    new ethereum.EventParam("pricePerTokenInWei", 
+    ethereum.Value.fromString("123"))
+  ]
+
+  mockRefreshContractCalls();
+  handleAddProject(call)
+  
+
+  assert.fieldEquals(ACCOUNT_ENTITY_TYPE, "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270", "id", "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270")
+  assert.fieldEquals(CONTRACT_ENTITY_TYPE, "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270", "admin", "0x90cba2bbb19ecc291a12066fd8329d65fa1f1947")
+  assert.fieldEquals(CONTRACT_ENTITY_TYPE, "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270", "renderProviderAddress", "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270")
+  assert.fieldEquals(PROJECT_ENTITY_TYPE, "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270-99", "projectId", "99")
+  assert.fieldEquals(PROJECT_ENTITY_TYPE, "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270-99", "active", "false")
+
+  clearStore();
+});
 
 // don't test handleUpdateAdmin -- function needs to interact with Core2?
 // test("Can update admin", () => {
@@ -218,7 +274,6 @@ test("Can update randomizer address", () => {
   mockRefreshContractCalls();
   
   handleUpdateRandomizerAddress(call)
-  logStore();
   assert.fieldEquals(CONTRACT_ENTITY_TYPE, "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270", "id", "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270")
   assert.fieldEquals(CONTRACT_ENTITY_TYPE, "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270", "randomizerContract", "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270")
 
@@ -267,7 +322,40 @@ test("Can update ArtBlocks percentage", () => {
   clearStore();
 }, true)
 
+// Project.load....I need to mock a full Project entity before I can refresh an existing script
+// test("Can handle add project script", () => {
+//   let call = changetype<AddProjectScriptCall>(newMockCall())
+  
+//   call.to = Address.fromString("0xa7d8d9ef8D8Ce8992Df33D8b8CF4Aebabd5bD270")  // must be 721Core?
+//   call.block.timestamp = BigInt.fromString('1230')
+//   let addr = Address.fromString("0x1233973F9aEa61250e98b697246cb10146903672")
+//   call.inputValues = [
+//     new ethereum.EventParam("_projectId",
+//     ethereum.Value.fromSignedBigInt(BigInt.fromString('10000')))
+//   ]
 
+//   // mockRefreshProjectScript();
+  
+//   handleAddProjectScript(call)
+  
+//   // assert.fieldEquals(CONTRACT_ENTITY_TYPE, "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270", "id", "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270")
+//   // assert.fieldEquals(CONTRACT_ENTITY_TYPE, "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270", "renderProviderPercentage", "10")
+
+//   clearStore();
+// }, true)
+
+//all these tests are calling Project.load()...mock that Entity
+// test("Can clear a Token IPFS image uri", () => {
+
+//   }
+
+// test("Can override token dynamic image with IPFS link", () => {
+
+//   }
+
+// test("Can remove a project's last script", () => {
+
+//   }
 
 
 // test("Can toggle projectIsActive", () => {
