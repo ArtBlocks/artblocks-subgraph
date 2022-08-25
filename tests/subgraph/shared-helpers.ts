a035185bed825465a8a841288b568beee89eaff6
ryley-o <30364988+ryley-o@users.noreply.github.com>
2022-08-25 14:23:22 -0700

    test patch for non-nullable externalAssetDependencyCount

--- tests/subgraph/shared-helpers.ts
+++ tests/subgraph/shared-helpers.ts
@@ -157,6 +157,7 @@ export class DefaultProjectValues {
   useHashString: boolean;
   useIpfs: boolean;
   website: string;
+  externalAssetDependencyCount: BigInt;
 }
 
 // These represent the values that would be returned by a
@@ -186,7 +187,8 @@ export const DEFAULT_PROJECT_VALUES: DefaultProjectValues = {
   scriptJSON: "",
   useHashString: true,
   useIpfs: false,
-  website: ""
+  website: "",
+  externalAssetDependencyCount: BigInt.zero()
 };
 
 // Store population functions
@@ -246,6 +248,7 @@ export const addNewProjectToStore = function(
   project.updatedAt = timestamp ? timestamp : CURRENT_BLOCK_TIMESTAMP;
   project.useHashString = DEFAULT_PROJECT_VALUES.useHashString;
   project.useIpfs = DEFAULT_PROJECT_VALUES.useIpfs;
+  project.externalAssetDependencyCount = BigInt.fromI32(0);
 
   project.save();
   return project;
