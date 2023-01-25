6147e776e59023d76aa952f0fba38c8a48dce2a4
ben lyaunzon <lyaunzon.b@gmail.com>
2023-01-25 10:37:23 -0800

    addressing further review comments

--- tests/subgraph/minter-suite/minter-suite-mapping.test.ts
+++ tests/subgraph/minter-suite/minter-suite-mapping.test.ts
@@ -3251,7 +3251,6 @@ describe("handleProjectMaxInvocationsLimitUpdated", () => {
     projectMinterConfig.currencyAddress = Address.zero();
     projectMinterConfig.currencySymbol = "ETH";
     projectMinterConfig.purchaseToDisabled = false;
-    projectMinterConfig.maxInvocations = null;
     projectMinterConfig.save();
 
     const event: ProjectMaxInvocationsLimitUpdated = changetype<
