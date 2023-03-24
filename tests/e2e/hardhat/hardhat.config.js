21d9a6937a05197377ae6b8199a8033c153655e0
benyo <yoshiwara.b@gmail.com>
2023-03-24 15:42:47 -0700

    Hardhat node task comment

--- tests/e2e/hardhat/hardhat.config.js
+++ tests/e2e/hardhat/hardhat.config.js
@@ -1,6 +1,11 @@
 require("@nomiclabs/hardhat-ethers");
 const fs = require("fs");
 
+/**
+ * This overrides the default hardhat task to write the 
+ * mnemonic to a file on a shardd volume so that it can 
+ * be used by the test runner.
+ */
 task('node', 'Starts a Hardhat node', async (args, hre, runSuper) => {
   const accounts = config.networks.hardhat.accounts;
   fs.writeFileSync("./shared/accounts.json", JSON.stringify({mnemonic: accounts.mnemonic}));
