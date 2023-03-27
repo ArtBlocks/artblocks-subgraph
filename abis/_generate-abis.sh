38473fe77da2019c3a9e32021609b4cebeccd1d8
ryley-o <30364988+ryley-o@users.noreply.github.com>
2023-03-27 13:00:21 -0700

    import minimal set of abis to abis directory to improve build time

---
+++ abis/_generate-abis.sh
@@ -0,0 +1,13 @@
+# clear existing ABIs
+rm -f ./*.json
+# import ABIs from artblocks npm dependency
+while IFS="" read -r contractName || [ -n "$contractName" ]
+do
+  # copy ABI json file from @artblocks npm dependency to the current directory
+  find '../node_modules/@artblocks/contracts/artifacts/contracts' -regex .*/$contractName.json | xargs -I {} cp {} .
+done < _include-artblocks-abis.txt
+# import abis from legacy ABIs directory
+cp ../abis-legacy/*.json .
+# import required openzeppelin ABIs
+cp ../node_modules/@openzeppelin-4.7/contracts/build/contracts/Ownable.json .
+cp ../node_modules/@openzeppelin-4.8/contracts-upgradeable/build/contracts/OwnableUpgradeable.json .
