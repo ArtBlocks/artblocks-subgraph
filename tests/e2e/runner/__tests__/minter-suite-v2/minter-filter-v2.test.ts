2f32eeb74ecdf82771bd45050e93e2c6f75a864b
ryley-o <30364988+ryley-o@users.noreply.github.com>
2023-06-09 10:43:22 -0700

    e2e tests for MinterApprovedGlobally MinterRevokedGlobally

--- tests/e2e/runner/__tests__/minter-suite-v2/minter-filter-v2.test.ts
+++ tests/e2e/runner/__tests__/minter-suite-v2/minter-filter-v2.test.ts
@@ -6,6 +6,10 @@ import {
   GetTargetCoreRegistriesDocument,
   GetTargetCoreRegistriesQuery,
   GetTargetCoreRegistriesQueryVariables,
+  GetTargetMintersDocument,
+  GetTargetMintersQuery,
+  GetTargetMintersQueryVariables,
+  Minter,
 } from "../../generated/graphql";
 import {
   getSubgraphConfig,
@@ -14,10 +18,20 @@ import {
   waitUntilSubgraphIsSynced,
 } from "../utils/helpers";
 
+import { MinterFilterV2__factory } from "../../contracts/factories/MinterFilterV2__factory";
+// @dev dummy shared minter used to test shared minter filter, but isn't used in production
+import { DummySharedMinter__factory } from "../../contracts/factories/DummySharedMinter__factory";
+import { DummySharedMinter } from "../../contracts";
+
 // waiting for subgraph to sync can take longer than the default 5s timeout
 jest.setTimeout(30 * 1000);
 
 const config = getSubgraphConfig();
+
+const client = createSubgraphClient();
+const { deployer, artist } = getAccounts();
+
+// set up contract instances and/or addresses
 const minterFilterAdminACLV0Address =
   config.metadata?.minterFilterAdminACLAddress;
 if (!minterFilterAdminACLV0Address)
@@ -33,12 +47,27 @@ const sharedMinterFilter = config.sharedMinterFilterContracts?.[0];
 if (!sharedMinterFilter) {
   throw new Error("No shared minter filter found in config metadata");
 }
+const sharedMinterFilterContract = new MinterFilterV2__factory(deployer).attach(
+  sharedMinterFilter.address
+);
 
-const client = createSubgraphClient();
-// const
-const { deployer, artist } = getAccounts();
+// helper functions
 
-describe("Expected contract exist", () => {
+// @dev deploys a new shared minter and waits for the subgraph to sync
+// @returns the deployed shared minter
+async function deployNewMinter(
+  sharedMinterFilterAddress: string
+): Promise<DummySharedMinter> {
+  const dummySharedMinterFactory = new DummySharedMinter__factory(deployer);
+  const dummySharedMinter = await dummySharedMinterFactory.deploy(
+    sharedMinterFilterAddress
+  );
+  await dummySharedMinter.deployed();
+  await waitUntilSubgraphIsSynced(client);
+  return dummySharedMinter;
+}
+
+describe("MinterFilterV2 event handling", () => {
   beforeAll(async () => {
     await waitUntilSubgraphIsSynced(client);
   });
@@ -88,6 +117,177 @@ describe("Expected contract exist", () => {
       // updated at not checked for exact value because its was not recorded during seed deployment
       // to be defined is sufficient
       expect(minterFilter?.updatedAt).toBeDefined();
+    });
+  });
+
+  describe("MinterApprovedGlobally", () => {
+    it("creates new Minter entity", async () => {
+      // deploy a new shared minter
+      const newMinter = await deployNewMinter(sharedMinterFilter.address);
+      // @dev minter does not yet exist because it is not in subgraph config
+      // approve minter globally
+      await sharedMinterFilterContract
+        .connect(deployer)
+        .approveMinterGlobally(newMinter.address);
+      await waitUntilSubgraphIsSynced(client);
+      // verify minter entity was created
+      const minterId = newMinter.address.toLowerCase();
+      const minterRes = (
+        await client
+          .query<GetTargetMintersQuery, GetTargetMintersQueryVariables>(
+            GetTargetMintersDocument,
+            { targetId: minterId }
+          )
+          .toPromise()
+      ).data?.minters[0];
+      if (!minterRes) throw new Error("No minter entity found");
+      expect(minterRes.id).toBe(minterId);
+    });
+
+    it("populates Minter entity as expected", async () => {
+      // deploy a new shared minter
+      const newMinter = await deployNewMinter(sharedMinterFilter.address);
+      // @dev minter does not yet exist because it is not in subgraph config
+      // approve minter globally
+      await sharedMinterFilterContract
+        .connect(deployer)
+        .approveMinterGlobally(newMinter.address);
+      await waitUntilSubgraphIsSynced(client);
+      // verify minter entity was created and is globally allowlisted
+      const minterId = newMinter.address.toLowerCase();
+      const minterRes = (
+        await client
+          .query<GetTargetMintersQuery, GetTargetMintersQueryVariables>(
+            GetTargetMintersDocument,
+            { targetId: minterId }
+          )
+          .toPromise()
+      ).data?.minters[0];
+      // verify minter fields were populated as expected
+      if (!minterRes) throw new Error("No minter entity found");
+      expect(minterRes.id).toBe(minterId);
+      expect(minterRes.type).toBe("DummySharedMinter");
+      expect(minterRes.minterFilter.id).toBe(
+        sharedMinterFilter.address.toLowerCase()
+      );
+      expect(minterRes.extraMinterDetails).toBe("{}");
+      expect(minterRes.isGloballyAllowlistedOnMinterFilter).toBe(true);
+    });
+
+    it("updates MinterFilter entity global allowlist", async () => {
+      // deploy a new shared minter
+      const newMinter = await deployNewMinter(sharedMinterFilter.address);
+      // @dev minter does not yet exist because it is not in subgraph config
+      // approve minter globally
+      await sharedMinterFilterContract
+        .connect(deployer)
+        .approveMinterGlobally(newMinter.address);
+      await waitUntilSubgraphIsSynced(client);
+      // verify minter filter global allowlist was updated
+      const minterFilterId = sharedMinterFilter.address.toLowerCase();
+      const minterFilterRes = (
+        await client
+          .query<
+            GetTargetMinterFiltersQuery,
+            GetTargetMinterFiltersQueryVariables
+          >(GetTargetMinterFiltersDocument, { targetId: minterFilterId })
+          .toPromise()
+      ).data?.minterFilters[0];
+      if (!minterFilterRes) throw new Error("No minter filter entity found");
+      expect(
+        minterFilterRes.minterGlobalAllowlist.map((minter) => minter.id)
+      ).toContain(newMinter.address.toLowerCase());
+      // derived field of knownMinters should also be updated
+      expect(minterFilterRes.knownMinters.map((minter) => minter.id)).toContain(
+        newMinter.address.toLowerCase()
+      );
+    });
+  });
+
+  describe("MinterRemovedFromGlobalAllowlist", () => {
+    it("updates Minter entity", async () => {
+      // deploy a new shared minter
+      const newMinter = await deployNewMinter(sharedMinterFilter.address);
+      // add minter to global allowlist
+      await sharedMinterFilterContract
+        .connect(deployer)
+        .approveMinterGlobally(newMinter.address);
+      await waitUntilSubgraphIsSynced(client);
+      // verify minter entity was updated to be globally allowlisted
+      const minterId = newMinter.address.toLowerCase();
+      const minterRes = (
+        await client
+          .query<GetTargetMintersQuery, GetTargetMintersQueryVariables>(
+            GetTargetMintersDocument,
+            { targetId: minterId }
+          )
+          .toPromise()
+      ).data?.minters[0];
+      if (!minterRes) throw new Error("No minter entity found");
+      expect(minterRes.id).toBe(minterId);
+      expect(minterRes.isGloballyAllowlistedOnMinterFilter).toBe(true);
+      // remove minter from global allowlist, verify minter entity was updated
+      await sharedMinterFilterContract
+        .connect(deployer)
+        .revokeMinterGlobally(newMinter.address);
+      await waitUntilSubgraphIsSynced(client);
+      const minterRes2 = (
+        await client
+          .query<GetTargetMintersQuery, GetTargetMintersQueryVariables>(
+            GetTargetMintersDocument,
+            { targetId: minterId }
+          )
+          .toPromise()
+      ).data?.minters[0];
+      if (!minterRes2) throw new Error("No minter entity found");
+      expect(minterRes2.id).toBe(minterId);
+      expect(minterRes2.isGloballyAllowlistedOnMinterFilter).toBe(false);
+    });
+
+    it("updates MinterFilter entity", async () => {
+      // deploy a new shared minter
+      const newMinter = await deployNewMinter(sharedMinterFilter.address);
+      // add minter to global allowlist
+      await sharedMinterFilterContract
+        .connect(deployer)
+        .approveMinterGlobally(newMinter.address);
+      await waitUntilSubgraphIsSynced(client);
+      // verify minter filter global allowlist contains new minter
+      const minterFilterId = sharedMinterFilter.address.toLowerCase();
+      const minterFilterRes = (
+        await client
+          .query<
+            GetTargetMinterFiltersQuery,
+            GetTargetMinterFiltersQueryVariables
+          >(GetTargetMinterFiltersDocument, { targetId: minterFilterId })
+          .toPromise()
+      ).data?.minterFilters[0];
+      if (!minterFilterRes) throw new Error("No minter filter entity found");
+      expect(
+        minterFilterRes.minterGlobalAllowlist.map((minter) => minter.id)
+      ).toContain(newMinter.address.toLowerCase());
+      // remove minter from global allowlist
+      await sharedMinterFilterContract
+        .connect(deployer)
+        .revokeMinterGlobally(newMinter.address);
+      await waitUntilSubgraphIsSynced(client);
+      // verify minter filter global allowlist does not contain new minter
+      const minterFilterRes2 = (
+        await client
+          .query<
+            GetTargetMinterFiltersQuery,
+            GetTargetMinterFiltersQueryVariables
+          >(GetTargetMinterFiltersDocument, { targetId: minterFilterId })
+          .toPromise()
+      ).data?.minterFilters[0];
+      if (!minterFilterRes2) throw new Error("No minter filter entity found");
+      expect(
+        minterFilterRes2.minterGlobalAllowlist.map((minter) => minter.id)
+      ).not.toContain(newMinter.address.toLowerCase());
+      // derived field of knownMinters should still contain the minter
+      expect(minterFilterRes.knownMinters.map((minter) => minter.id)).toContain(
+        newMinter.address.toLowerCase()
+      );
     });
   });
 });
