bb064b80f0c386c08d7946d4f7f1928b5c01551f
ryley-o <30364988+ryley-o@users.noreply.github.com>
2023-06-21 20:52:12 -0700

    add initial merkle-specific handlers

---
+++ src/i-shared-merkle-mapping.ts
@@ -0,0 +1,48 @@
+import {
+  DefaultMaxInvocationsPerAddress,
+  DelegationRegistryUpdated
+} from "../generated/ISharedMerkle/IFilteredSharedMerkle";
+
+import { loadOrCreateMinter } from "./helpers";
+
+import { setMinterExtraMinterDetailsValue } from "./extra-minter-details-helpers";
+
+///////////////////////////////////////////////////////////////////////////////
+// EVENT HANDLERS start here
+///////////////////////////////////////////////////////////////////////////////
+
+export function handleDefaultMaxInvocationsPerAddress(
+  event: DefaultMaxInvocationsPerAddress
+): void {
+  // load minter
+  const minter = loadOrCreateMinter(event.address, event.block.timestamp);
+
+  // update minter extra minter details value
+  setMinterExtraMinterDetailsValue(
+    "defaultMaxInvocationsPerAddress",
+    event.params.defaultMaxInvocationsPerAddress,
+    minter
+  );
+
+  // update minter's updatedAt timestamp to induce a sync, and save
+  minter.updatedAt = event.block.timestamp;
+  minter.save();
+}
+
+export function handleDelegationRegistryUpdated(
+  event: DelegationRegistryUpdated
+): void {
+  // load minter
+  const minter = loadOrCreateMinter(event.address, event.block.timestamp);
+
+  // update minter extra minter details value
+  setMinterExtraMinterDetailsValue(
+    "delegationRegistryAddress",
+    event.params.delegationRegistry,
+    minter
+  );
+
+  // update minter's updatedAt timestamp to induce a sync, and save
+  minter.updatedAt = event.block.timestamp;
+  minter.save();
+}
