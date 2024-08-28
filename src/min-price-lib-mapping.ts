446bd39049d2584277db1a56f1433645ea339ecc
ryley-o <30364988+ryley-o@users.noreply.github.com>
2024-08-28 12:56:21 -0700

    update handler logic and event templating

---
+++ src/min-price-lib-mapping.ts
@@ -0,0 +1,35 @@
+import { DefaultMintFeeUpdated } from "../generated/MinPriceLib/MinPriceLib";
+import { loadOrCreateMinter } from "./helpers";
+import { setMinterExtraMinterDetailsValue } from "./extra-minter-details-helpers";
+
+///////////////////////////////////////////////////////////////////////////////
+// EVENT HANDLERS start here
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Handles the update of contract-level default mint fee.
+ * Loads or creates the minter, updates the minter's extra minter details with
+ * the updated value, and induces a sync of the minter entity.
+ * @param event The event carrying new default mint fee value.
+ */
+export function handleDefaultMintFeeUpdated(
+  event: DefaultMintFeeUpdated
+): void {
+  // load minter
+  const minter = loadOrCreateMinter(event.address, event.block.timestamp);
+
+  // update minter entity
+  setMinterExtraMinterDetailsValue(
+    "defaultMintFee",
+    event.params.defaultMintFee.toString(),
+    minter
+  );
+
+  // update minter's updatedAt timestamp to induce a sync, and save
+  minter.updatedAt = event.block.timestamp;
+  minter.save();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// EVENT HANDLERS end here
+///////////////////////////////////////////////////////////////////////////////
