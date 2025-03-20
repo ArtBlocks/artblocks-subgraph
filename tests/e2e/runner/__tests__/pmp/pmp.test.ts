88a0babc4d7862b0e24c3310433e04d33f5220ed
Lindsay Gilbert <lindsay@artblocks.io>
2025-03-20 11:10:25 -0400

    more tests

--- tests/e2e/runner/__tests__/pmp/pmp.test.ts
+++ tests/e2e/runner/__tests__/pmp/pmp.test.ts
@@ -93,6 +93,7 @@ describe("PMP event handling", () => {
       );
 
       const projectPmpConfig = projectPmpConfigs?.[0];
+      expect(projectPmpConfig?.pmpProjectConfig?.id).toBe(targetId);
       expect(projectPmpConfig?.key).toBe(paramKey);
       expect(projectPmpConfig?.authOption).toBe("ArtistAndTokenOwner");
       expect(projectPmpConfig?.paramType).toBe("HexColor");
@@ -100,11 +101,15 @@ describe("PMP event handling", () => {
       expect(projectPmpConfig?.authAddress).toBe(
         ethers.constants.AddressZero.toLowerCase()
       );
+      expect(projectPmpConfig?.selectOptions).toEqual([]);
       expect(projectPmpConfig?.minRange).toBe(
         "0x0000000000000000000000000000000000000000000000000000000000000000"
       );
       expect(projectPmpConfig?.maxRange).toBe(
         "0x0000000000000000000000000000000000000000000000000000000000000000"
+      );
+      expect(projectPmpConfig?.createdAt).toBe(
+        projectConfiguredTimestamp.toString()
       );
     });
   });
@@ -112,6 +117,7 @@ describe("PMP event handling", () => {
     const projectId = 0;
     // first configuration
     const firstParamKey = "background";
+    const secondParamKey = "foreground";
     let tx = await pmpV0Contract
       .connect(artist)
       .configureProject(genArt721CoreAddress, projectId, [
@@ -129,17 +135,90 @@ describe("PMP event handling", () => {
               "0x0000000000000000000000000000000000000000000000000000000000000000",
           },
         },
+        {
+          key: secondParamKey,
+          pmpConfig: {
+            authOption: 1, // TokenOwner auth
+            paramType: 6, // HexColor type
+            pmpLockedAfterTimestamp: 0,
+            authAddress: ethers.constants.AddressZero,
+            selectOptions: [],
+            minRange:
+              "0x0000000000000000000000000000000000000000000000000000000000000000",
+            maxRange:
+              "0x0000000000000000000000000000000000000000000000000000000000000000",
+          },
+        },
       ]);
-    await tx.wait();
+
+    const receiptOne = await tx.wait();
     await waitUntilSubgraphIsSynced(client);
 
+    const projectConfiguredTimestamp = (
+      await artist.provider.getBlock(receiptOne.blockNumber)
+    )?.timestamp;
+
+    // validate first configuration
+    const targetId = `${pmpV0Address.toLowerCase()}-${genArt721CoreAddress.toLowerCase()}-${projectId}`;
+    const projectPmpConfigRes = await getProjectPMPConfigDetails(
+      client,
+      targetId
+    );
+    const fullProjectId = genArt721CoreAddress.toLowerCase().concat("-0");
+    const projectRes = await getProjectDetails(client, fullProjectId);
+
+    // verify both param configs exist
+    const projectPmpConfigs = projectPmpConfigRes?.pmpConfigs;
+    expect(projectPmpConfigs?.length).toBe(2);
+    expect(projectPmpConfigRes?.pmpConfigCount).toBe("2");
+
+    // verify the new config details
+    const firstConfig = projectPmpConfigs?.[0];
+    expect(firstConfig?.pmpProjectConfig?.id).toBe(targetId);
+    expect(firstConfig?.key).toBe(firstParamKey);
+    expect(firstConfig?.authOption).toBe("ArtistAndTokenOwner");
+    expect(firstConfig?.paramType).toBe("HexColor");
+    expect(firstConfig?.selectOptions).toEqual([]);
+
+    expect(firstConfig?.minRange).toBe(
+      "0x0000000000000000000000000000000000000000000000000000000000000000"
+    );
+    expect(firstConfig?.maxRange).toBe(
+      "0x0000000000000000000000000000000000000000000000000000000000000000"
+    );
+    expect(firstConfig?.createdAt).toBe(projectConfiguredTimestamp.toString());
+    expect(projectPmpConfigRes?.updatedAt).toBe(
+      projectConfiguredTimestamp.toString()
+    );
+
+    const secondConfig = projectPmpConfigs?.[1];
+    expect(secondConfig?.pmpProjectConfig?.id).toBe(targetId);
+    expect(secondConfig?.key).toBe(secondParamKey);
+    expect(secondConfig?.authOption).toBe("TokenOwner");
+    expect(secondConfig?.paramType).toBe("HexColor");
+    expect(secondConfig?.selectOptions).toEqual([]);
+    expect(secondConfig?.minRange).toBe(
+      "0x0000000000000000000000000000000000000000000000000000000000000000"
+    );
+    expect(secondConfig?.maxRange).toBe(
+      "0x0000000000000000000000000000000000000000000000000000000000000000"
+    );
+    expect(secondConfig?.createdAt).toBe(projectConfiguredTimestamp.toString());
+
+    const firstPMPConfigKey = `${pmpV0Address.toLowerCase()}-${genArt721CoreAddress.toLowerCase()}-${projectId}-${firstParamKey}`;
+    const secondPMPConfigKey = `${pmpV0Address.toLowerCase()}-${genArt721CoreAddress.toLowerCase()}-${projectId}-${firstParamKey}`;
+    expect(projectPmpConfigRes?.pmpConfigKeys).toContain(firstPMPConfigKey);
+    expect(projectPmpConfigRes?.pmpConfigKeys).toContain(secondPMPConfigKey);
+    // validate project field
+    expect(projectRes?.pmpProjectConfig?.id).toBe(targetId);
+
     // re-configure with different parameter
-    const secondParamKey = "size";
+    const newParamKey = "size";
     tx = await pmpV0Contract
       .connect(artist)
       .configureProject(genArt721CoreAddress, projectId, [
         {
-          key: secondParamKey,
+          key: newParamKey,
           pmpConfig: {
             authOption: 3, // ArtistAndTokenOwner auth
             paramType: 3, // Uint256Range type
@@ -153,40 +232,53 @@ describe("PMP event handling", () => {
           },
         },
       ]);
-    const receipt = await tx.wait();
-    const projectConfiguredTimestamp = (
-      await artist.provider.getBlock(receipt.blockNumber)
+    const receiptTwo = await tx.wait();
+    const projectConfiguredNextTimestamp = (
+      await artist.provider.getBlock(receiptTwo.blockNumber)
     )?.timestamp;
 
     await waitUntilSubgraphIsSynced(client);
-    const targetId = `${pmpV0Address.toLowerCase()}-${genArt721CoreAddress.toLowerCase()}-${projectId}`;
-    const projectPmpConfigRes = await getProjectPMPConfigDetails(
+
+    const projectPmpConfigNextRes = await getProjectPMPConfigDetails(
       client,
       targetId
     );
+    const projectNextRes = await getProjectDetails(client, fullProjectId);
 
     // verify only one config exists and it's the new one
-    const projectPmpConfigs = projectPmpConfigRes?.pmpConfigs;
-    expect(projectPmpConfigs?.length).toBe(1);
-    expect(projectPmpConfigRes?.pmpConfigCount).toBe("1");
+    const projectPmpConfigsNext = projectPmpConfigNextRes?.pmpConfigs;
+    expect(projectPmpConfigsNext?.length).toBe(1);
+    expect(projectPmpConfigNextRes?.pmpConfigCount).toBe("1");
 
     // verify the new config details
-    const newConfig = projectPmpConfigs?.[0];
-    expect(newConfig?.key).toBe(secondParamKey);
-    expect(newConfig?.authOption).toBe("ArtistAndTokenOwner");
-    expect(newConfig?.paramType).toBe("Uint256Range");
-    expect(newConfig?.minRange).toBe(
+    const nextConfig = projectPmpConfigsNext?.[0];
+    expect(nextConfig?.pmpProjectConfig?.id).toBe(targetId);
+    expect(nextConfig?.key).toBe(newParamKey);
+    expect(nextConfig?.authOption).toBe("ArtistAndTokenOwner");
+    expect(nextConfig?.paramType).toBe("Uint256Range");
+    expect(nextConfig?.selectOptions).toEqual([]);
+    expect(nextConfig?.minRange).toBe(
       "0x0000000000000000000000000000000000000000000000000000000000000001"
     );
-    expect(newConfig?.maxRange).toBe(
+    expect(nextConfig?.maxRange).toBe(
       "0x000000000000000000000000000000000000000000000000000000000000000a"
     );
-    expect(projectPmpConfigRes?.updatedAt).toBe(
-      projectConfiguredTimestamp.toString()
+    expect(nextConfig?.createdAt).toBe(
+      projectConfiguredNextTimestamp.toString()
+    );
+    expect(projectPmpConfigNextRes?.updatedAt).toBe(
+      projectConfiguredNextTimestamp.toString()
     );
 
     // verify the old config key is no longer in the keys array
     const oldPmpConfigKey = `${pmpV0Address.toLowerCase()}-${genArt721CoreAddress.toLowerCase()}-${projectId}-${firstParamKey}`;
-    expect(projectPmpConfigRes?.pmpConfigKeys).not.toContain(oldPmpConfigKey);
+    const newPmpConfigKey = `${pmpV0Address.toLowerCase()}-${genArt721CoreAddress.toLowerCase()}-${projectId}-${newParamKey}`;
+    expect(projectPmpConfigNextRes?.pmpConfigKeys).toContain(newPmpConfigKey);
+    expect(projectPmpConfigNextRes?.pmpConfigKeys).not.toContain(
+      oldPmpConfigKey
+    );
+
+    // validate project field
+    expect(projectNextRes?.pmpProjectConfig?.id).toBe(targetId);
   });
 });
